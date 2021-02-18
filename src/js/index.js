ce8258c89d1a694cd415132cbba53910f728d4e0
Daniel Chmur <daniel.chmur@gmail.com>
2021-02-18 18:41:27 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import "@babel/polyfill";
+
+const form = document.getElementById("form");
+const preview = document.getElementById("preview");
+
+const inputs = document.querySelectorAll("select,input");
+for (let input of inputs) {
+	input.oninput = show;
+}
+
+function show(event) {
+	preview.textContent = form.text.value;
+	preview.style.padding = "10px";
+	preview.style.margin = "10px";
+}
+
+show();
